f2a53c96140fc135e74daa7663fc059c6a214336
theborg3of5 <theborg3of5@gmail.com>
2013-09-15 19:22:29 -0500

    Initial fill of contents, also potential finish.

---
+++ background.js
@@ -0,0 +1,14 @@
+﻿// Upon tab creation, grab the parent tab by its ID, and copy over the history.
+function tabCreated(childTab) {
+	// If the new tab isn't empty, don't move it - it's probably already next to the tab we want.
+	if(childTab.url === "chrome://newtab/") {
+		// Get the parent's index.
+		chrome.tabs.get(childTab.openerTabId, function(parentTab) {
+			// Stick the new tab right next to it.
+			chrome.tabs.move(childTab.id, {"index": parentTab.index+1});
+		});
+	}
+}
+
+// Register it to happen when a new tab opens.
+chrome.tabs.onCreated.addListener(tabCreated);
