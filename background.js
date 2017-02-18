10db820998f8dd5d628189cb15b1436096d7a903
Gavin Borg <theborg3of5@gmail.com>
2017-02-18 15:51:02 -0600

    Rewrite to make code cleaner. Also support duplicated tabs now.

--- background.js
+++ background.js
@@ -1,14 +1,92 @@
-﻿// Upon tab creation, grab the parent tab by its ID, and copy over the history.
-function tabCreated(childTab) {
-	// If the new tab isn't empty, don't move it - it's probably already next to the tab we want.
-	if(childTab.url === "chrome://newtab/") {
-		// Get the parent's index.
-		chrome.tabs.get(childTab.openerTabId, function(parentTab) {
-			// Stick the new tab right next to it.
-			chrome.tabs.move(childTab.id, {"index": parentTab.index+1});
-		});
-	}
+﻿var ChromeNewTabURL = "chrome://newtab/";
+
+
+function moveTabIfNeeded(tab) {
+	moveIfDefaultNewTab(tab);
+	moveIfDuplicatedTab(tab);
 }
 
-// Register it to happen when a new tab opens.
-chrome.tabs.onCreated.addListener(tabCreated);
+function moveIfDefaultNewTab(tab) {
+	if(!isDefaultNewTab(tab))
+		return;
+	
+	moveTabNextToParent(tab);
+}
+function isDefaultNewTab(tab) {
+	if(!tab)
+		return false;
+	
+	return (tab.url == ChromeNewTabURL);
+}
+
+function moveIfDuplicatedTab(tab) {
+	var parentId = getTabParentId(tab);
+	if(!parentId)
+		return;
+	
+	chrome.tabs.get(
+		parentId,
+		function(parentTab) {
+			if(!gotTabOK(parentTab))
+				return;
+			if(tab.url != parentTab.url)
+				return;
+			
+			moveTabNextToParent(tab);
+		}
+	);
+}
+
+function moveTabNextToParent(tab) {
+	var parentId = getTabParentId(tab);
+	if(!parentId)
+		return;
+	
+	chrome.tabs.get(
+		parentId,
+		function(parentTab) {
+			if(!gotTabOK(parentTab))
+				return;
+			
+			moveTabNextToOtherTab(tab, parentTab);
+		}
+	);
+}
+function moveTabNextToOtherTab(tab, otherTab) {
+	if(!otherTab)
+		return;
+	
+	moveTabToIndex(tab, otherTab.index + 1);
+}
+function moveTabToIndex(tab, index) {
+	if(!tab)
+		return;
+	
+	chrome.tabs.move(
+		tab.id, 
+		{
+			"index": index
+		}
+	);
+}
+
+function getTabParentId(tab) {
+	if(!tab)
+		return "";
+	
+	return tab.openerTabId;
+}
+
+function gotTabOK(tab) {
+	var lastError = chrome.runtime.lastError; // Check lastError so Chrome doesn't output anything to the console.
+	if(lastError)
+		return false;
+	
+	if(!tab)
+		return false;
+	
+	return true;
+}
+
+
+chrome.tabs.onCreated.addListener(moveTabIfNeeded);
