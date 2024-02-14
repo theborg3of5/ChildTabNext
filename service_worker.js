ea85cc3a8b71716b35ea4c4d804d3b95f2660dd6
Gavin Borg <theborg3of5@gmail.com>
2024-02-13 21:13:08 -0600

    Clean out the unused stuff, finish documentation.

--- service_worker.js
+++ service_worker.js
@@ -1,8 +1,8 @@
 ﻿
 /**
- * Whenever a new tab is created, check if we need to move it (and do so if so).
+ * Whenever a new tab is created, check if we need to move it (and if we do, take care of it).
  * https://developer.chrome.com/docs/extensions/reference/api/tabs#event-onCreated
- * @param {Tab} tab Tab that was created.
+ * @param {Tab} tab The tab that was just created.
  */
 chrome.tabs.onCreated.addListener(async (tab) =>
 {
@@ -14,35 +14,17 @@ chrome.tabs.onCreated.addListener(async (tab) =>
 		return;
 	
 	if (needToMoveTab(tab, parentTab))
-	{ 
-		// moveTabNextToOtherTab(tab, parentTab);
+	{
 		chrome.tabs.move(tab.id, { "index": parentTab.index + 1 });
 	}
-	
-
-	// // moveIfDefaultNewTab(tab);
-	// if ( (tab.url === "chrome://newtab/") || (tab.pendingUrl === "chrome://newtab/") )
-	// 	moveTabNextToParent(tab);
-	// //return
-
-	// // moveIfDuplicatedTab(tab);
-	// var parentId = tab.openerTabId;
-	// if(!parentId)
-	// 	return;
-	
-	// chrome.tabs.get(
-	// 	parentId,
-	// 	function(parentTab) {
-	// 		if(!gotTabOK(parentTab))
-	// 			return;
-	// 		if(tab.url != parentTab.url)
-	// 			return;
-			
-	// 		moveTabNextToParent(tab);
-	// 	}
-	// );
 });
 
+/**
+ * Determine whether we should move the new tab to be next to its parent.
+ * @param {Tab} tab The tab we might want to move.
+ * @param {Tab} parentTab The parent (opener) of tab.
+ * @returns true/false - do we need to move the tab?
+ */
 function needToMoveTab(tab, parentTab)
 {
 	// New tabs
@@ -57,60 +39,3 @@ function needToMoveTab(tab, parentTab)
 
 	return false;
 }
-
-// function moveIfDefaultNewTab(tab) {
-// 	if (!tab)
-// 		return;
-	
-// 	if ( (tab.url === "chrome://newtab/") || (tab.pendingUrl === "chrome://newtab/") )
-// 		moveTabNextToParent(tab);
-// }
-
-// function moveIfDuplicatedTab(tab) {
-	
-// }
-
-// function moveTabNextToParent(tab) {
-// 	var parentId = tab.openerTabId;
-// 	if(!parentId)
-// 		return;
-	
-// 	chrome.tabs.get(
-// 		parentId,
-// 		function(parentTab) {
-// 			if(!gotTabOK(parentTab))
-// 				return;
-			
-// 			moveTabNextToOtherTab(tab, parentTab);
-// 		}
-// 	);
-// }
-// function moveTabNextToOtherTab(tab, otherTab) {
-// 	if(!otherTab)
-// 		return;
-	
-// 	// moveTabToIndex(tab, otherTab.index + 1);
-// 	chrome.tabs.move(tab.id, { "index": otherTab.index + 1 });
-// }
-// function moveTabToIndex(tab, index) {
-// 	if(!tab)
-// 		return;
-	
-// 	chrome.tabs.move(
-// 		tab.id, 
-// 		{
-// 			"index": index
-// 		}
-// 	);
-// }
-
-// function gotTabOK(tab) {
-// 	var lastError = chrome.runtime.lastError; // Check lastError so Chrome doesn't output anything to the console.
-// 	if(lastError)
-// 		return false;
-	
-// 	if(!tab)
-// 		return false;
-	
-// 	return true;
-// }
