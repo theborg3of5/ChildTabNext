7b8c6c811c4f1d4bd3c2bef7110a5373ed2d9925
Gavin Borg <theborg3of5@gmail.com>
2024-02-13 21:10:07 -0600

    Massive strip-down of code. Still appears to be fully functional.

--- service_worker.js
+++ service_worker.js
@@ -4,91 +4,113 @@
  * https://developer.chrome.com/docs/extensions/reference/api/tabs#event-onCreated
  * @param {Tab} tab Tab that was created.
  */
-chrome.tabs.onCreated.addListener((tab) =>
-{ 
-	moveIfDefaultNewTab(tab);
-	moveIfDuplicatedTab(tab);
-});
-
-function moveIfDefaultNewTab(tab) {
+chrome.tabs.onCreated.addListener(async (tab) =>
+{
 	if (!tab)
 		return;
 	
-	if ( (tab.url === "chrome://newtab/") || (tab.pendingUrl === "chrome://newtab/") )
-		moveTabNextToParent(tab);
-}
-function isDefaultNewTab(tab) {
-	if(!tab)
-		return false;
-	
-	return (tab.url || tab.pendingUrl) === "chrome://newtab/";
-}
-
-function moveIfDuplicatedTab(tab) {
-	var parentId = getTabParentId(tab);
-	if(!parentId)
+	const parentTab = await chrome.tabs.get(tab.openerTabId);
+	if (!parentTab)
 		return;
 	
-	chrome.tabs.get(
-		parentId,
-		function(parentTab) {
-			if(!gotTabOK(parentTab))
-				return;
-			if(tab.url != parentTab.url)
-				return;
+	if (needToMoveTab(tab, parentTab))
+	{ 
+		// moveTabNextToOtherTab(tab, parentTab);
+		chrome.tabs.move(tab.id, { "index": parentTab.index + 1 });
+	}
+	
+
+	// // moveIfDefaultNewTab(tab);
+	// if ( (tab.url === "chrome://newtab/") || (tab.pendingUrl === "chrome://newtab/") )
+	// 	moveTabNextToParent(tab);
+	// //return
+
+	// // moveIfDuplicatedTab(tab);
+	// var parentId = tab.openerTabId;
+	// if(!parentId)
+	// 	return;
+	
+	// chrome.tabs.get(
+	// 	parentId,
+	// 	function(parentTab) {
+	// 		if(!gotTabOK(parentTab))
+	// 			return;
+	// 		if(tab.url != parentTab.url)
+	// 			return;
 			
-			moveTabNextToParent(tab);
-		}
-	);
+	// 		moveTabNextToParent(tab);
+	// 	}
+	// );
+});
+
+function needToMoveTab(tab, parentTab)
+{
+	// New tabs
+	if (tab.url === "chrome://newtab/")
+		return true;
+	if (tab.pendingUrl === "chrome://newtab/")
+		return true;
+
+	// Duplicated tabs
+	if (tab.url === parentTab.url)
+		return true;
+
+	return false;
 }
 
-function moveTabNextToParent(tab) {
-	var parentId = getTabParentId(tab);
-	if(!parentId)
-		return;
+// function moveIfDefaultNewTab(tab) {
+// 	if (!tab)
+// 		return;
 	
-	chrome.tabs.get(
-		parentId,
-		function(parentTab) {
-			if(!gotTabOK(parentTab))
-				return;
+// 	if ( (tab.url === "chrome://newtab/") || (tab.pendingUrl === "chrome://newtab/") )
+// 		moveTabNextToParent(tab);
+// }
+
+// function moveIfDuplicatedTab(tab) {
+	
+// }
+
+// function moveTabNextToParent(tab) {
+// 	var parentId = tab.openerTabId;
+// 	if(!parentId)
+// 		return;
+	
+// 	chrome.tabs.get(
+// 		parentId,
+// 		function(parentTab) {
+// 			if(!gotTabOK(parentTab))
+// 				return;
 			
-			moveTabNextToOtherTab(tab, parentTab);
-		}
-	);
-}
-function moveTabNextToOtherTab(tab, otherTab) {
-	if(!otherTab)
-		return;
+// 			moveTabNextToOtherTab(tab, parentTab);
+// 		}
+// 	);
+// }
+// function moveTabNextToOtherTab(tab, otherTab) {
+// 	if(!otherTab)
+// 		return;
 	
-	moveTabToIndex(tab, otherTab.index + 1);
-}
-function moveTabToIndex(tab, index) {
-	if(!tab)
-		return;
+// 	// moveTabToIndex(tab, otherTab.index + 1);
+// 	chrome.tabs.move(tab.id, { "index": otherTab.index + 1 });
+// }
+// function moveTabToIndex(tab, index) {
+// 	if(!tab)
+// 		return;
 	
-	chrome.tabs.move(
-		tab.id, 
-		{
-			"index": index
-		}
-	);
-}
+// 	chrome.tabs.move(
+// 		tab.id, 
+// 		{
+// 			"index": index
+// 		}
+// 	);
+// }
 
-function getTabParentId(tab) {
-	if(!tab)
-		return "";
+// function gotTabOK(tab) {
+// 	var lastError = chrome.runtime.lastError; // Check lastError so Chrome doesn't output anything to the console.
+// 	if(lastError)
+// 		return false;
 	
-	return tab.openerTabId;
-}
-
-function gotTabOK(tab) {
-	var lastError = chrome.runtime.lastError; // Check lastError so Chrome doesn't output anything to the console.
-	if(lastError)
-		return false;
+// 	if(!tab)
+// 		return false;
 	
-	if(!tab)
-		return false;
-	
-	return true;
-}
+// 	return true;
+// }
